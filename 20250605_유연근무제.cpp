87322abe9496ed5bb4e041871b2ef2087d460396
kimsewone <lmaya3@naver.com>
2025-06-06 03:21:37 +0900

    Update 20250605_유연근무제.cpp

--- 20250605_유연근무제.cpp
+++ 20250605_유연근무제.cpp
@@ -16,34 +16,25 @@ int add10min(int time) {
     return hour * 100 + min;
 }
 
-
 int getDay(int startday, int offset) {
     return ((startday - 1 + offset) % 7) + 1;
 }
 
-
 int solution(int schedules[], size_t schedules_len, int** timelogs, size_t timelogs_rows, size_t timelogs_cols, int startday) {
     int answer = 0;
-
     for (int i = 0; i < schedules_len; i++) {
         int limit = add10min(schedules[i]);
         bool onTimeAllWeek = true;
-
         for (int j = 0; j < 7; j++) {
-            int day = getDay(startday, j);
-
-            
+            int day = getDay(startday, j); 
             if (day == 6 || day == 7) continue;
-
             int arrive = timelogs[i][j];
             if (arrive > limit) {
                 onTimeAllWeek = false;
                 break; 
             }
         }
-
         if (onTimeAllWeek) answer++;
     }
-
     return answer;
 }
